f8146bdc3bb646984cf9d4eb3031d8f5ed351b79
lucasamsilva <lucasandreim280998@gmail.com>
2020-10-28 10:25:40 -0300

    Semana 1

---
+++ Semana 1/EstruturaSimples2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <malloc.h>
+#define alturaMaxima 225
+
+typedef struct
+{
+  int peso;   // peso em quilogramas
+  int altura; // altura em centimetros
+} PesoAltura;
+
+int main() {
+  int x;
+  PesoAltura* pessoa1;
+  printf("Valor inicial do endereco: %p\n", pessoa1);
+  pessoa1 = (PesoAltura*) malloc(sizeof(PesoAltura));
+  printf("Peso: %i, Altura %i. ", pessoa1->peso, pessoa1->altura);
+  pessoa1->peso = 80;
+  pessoa1->altura = 185;
+
+  printf("Peso: %i, Altura %i. ", pessoa1->peso, pessoa1->altura);
+  if (pessoa1->altura>alturaMaxima) {
+    printf("Altura acima da maxima.\n");
+  }
+  else printf("Altura abaixo da maxima.\n");
+
+  printf("Enderecos: %p %p %p\n", &x, &pessoa1, pessoa1);
+ 
+  return 0;
+}
